eae243a2fedd37c9068bba16d6154dff89004b6a
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-10 15:51:18 +0100

    added invitation array & simple layout

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,26 @@
-console.log("JS ok");
+// array di invitati
+// chiedere nome
+// display messaggio permesso
+
+const guestList = [
+  "Daniel E. Zambrana",
+  "Jeffrey H. Paul",
+  "Darlene J. Vogler",
+  "Frances J. Bertram",
+  "Sharon R. Anderson",
+  "Paul J. Sanders",
+  "Leslie R. Brubaker",
+  "Andrew L. Parker",
+  "Jennifer B. Serrano",
+  "Scott C. Gaspar",
+  "Paul M. Dole",
+  "Georgia K. Stutler",
+  "Debbie P. Miller",
+  "Kyle E. Hunt",
+  "Echo E. Bell",
+  "Clinton I. O'Connor",
+  "Martha T. Owens",
+  "Erica J. Rogers",
+  "Phyllis R. Pierce",
+  "Michele M. Johnson",
+];
