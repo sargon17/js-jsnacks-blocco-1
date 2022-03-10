a549598a85cbc7f8cb38930435feee94d8b853e8
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-10 17:00:46 +0100

    3rd es done

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,11 @@
-console.log("JS ok");
+const number = prompt("Input any lenght number:");
+
+// let n = String(number).slice(0, 1);
+
+let sum = 0;
+for (let i = 0; i < number.length; i++) {
+  let n = String(number).slice(i, i + 1);
+  sum += parseInt(n);
+}
+
+console.log(sum);
