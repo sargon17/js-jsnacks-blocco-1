0669be59dda8b8d4f70959463f198a7e775927de
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-10 16:36:52 +0100

    jsnack2 done

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,9 @@
-console.log("JS ok");
+const oddNumbers = [];
+
+for (let i = 0; i < 6; i++) {
+  let number = prompt("Input a number:");
+  if (number % 2 === 0) {
+    oddNumbers.push(number);
+  }
+}
+console.log(oddNumbers);
