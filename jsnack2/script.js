cde7d992b3571d1a19bd038fdffcd28d49b83195
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-10 17:50:02 +0100

    2 ex bug solved

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,7 +2,7 @@ const oddNumbers = [];
 
 for (let i = 0; i < 6; i++) {
   let number = prompt("Input a number:");
-  if (number % 2 === 0) {
+  if (number % 2 !== 0) {
     oddNumbers.push(number);
   }
 }
